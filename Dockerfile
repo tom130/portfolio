0b4a180d286ad33915c498edf423215475e8e3a4
tomas.krbec@external.telekom.de <tomas.krbec@external.telekom.de>
2024-07-02 11:40:06 +0200

    feat: updated with my info
    Signed-off-by: tomas.krbec@external.telekom.de <tomas.krbec@external.telekom.de>

---
+++ Dockerfile
@@ -0,0 +1,17 @@
+FROM node:18.18.0 AS builder
+WORKDIR /app
+COPY package.json yarn.lock ./
+RUN yarn install
+COPY . .
+RUN yarn build
+
+# CREATE FINAL NGINX IMAGE
+
+FROM nginx:latest
+EXPOSE $NGINX_PORT
+COPY --from=builder /build /usr/share/nginx/html
+RUN mkdir /config &&\
+  rm /etc/nginx/conf.d/default.conf &&\
+  ln -s /config/custom.conf /etc/nginx/conf.d/default.conf
+CMD ["nginx", "-g", "daemon off;"]
+
--- src/components/Home/Type.js
+++ src/components/Home/Type.js
@@ -3,19 +3,18 @@ import Typewriter from "typewriter-effect";
 
 function Type() {
   return (
-    <Typewriter
-      options={{
-        strings: [
-          "Software Developer",
-          "Freelancer",
-          "MERN Stack Developer",
-          "Open Source Contributor",
-        ],
-        autoStart: true,
-        loop: true,
-        deleteSpeed: 50,
-      }}
-    />
+      <Typewriter
+          options={{
+            strings: [
+              "Fullstack Software Developer",
+              "DevOPS Engineer",
+              "Freelancer",
+            ],
+            autoStart: true,
+            loop: true,
+            deleteSpeed: 50,
+          }}
+      />
   );
 }
 
